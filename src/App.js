31cdb7717ccdf8abb905d7984d83efcee07adacd
Alex Kharouk <akharoukpp@gmail.com>
2021-07-06 18:31:10 +0100

    create transactions on test environment

--- src/App.js
+++ src/App.js
@@ -6,12 +6,33 @@ import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 function App() {
-  const [greeting, setGreetingVal] = useState();
+  const [greeting, setGreetingVal] = useState('');
   const [displayGreeting, setDisplayGreeting] = useState('Connecting...');
 
   async function requestAccount() {
     // prompts the user to connect to Web3 via Metamask
     await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  async function setGreeting() {
+    if (!greeting) return;
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      // when we create an update to the blockchain, we need to have a signer
+      // so that we can sign the transaction
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      console.log({
+        signer,
+        contract,
+        transaction,
+      });
+      setGreetingVal('');
+      await transaction.wait(); // could take a while to write to the production blockchain
+      fetchGreeting();
+    }
   }
 
   async function fetchGreeting() {
@@ -35,6 +56,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>{displayGreeting}</h1>
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <br />
+        <input
+          onChange={(e) => setGreetingVal(e.target.value)}
+          value={greeting}
+        />
+        <button onClick={setGreeting}>Set Greeting</button>
       </header>
     </div>
   );
