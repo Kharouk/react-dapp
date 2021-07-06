f8a0544f3bdc6e1f90feb708812b05ec1e659af9
Alex Kharouk <akharoukpp@gmail.com>
2021-07-06 16:28:04 +0100

    set up react to account to ethers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
 import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 function App() {
+  const [greeting, setGreetingVal] = useState();
+
+  async function requestAccount() {}
+  async function fetchGreeting() {}
+  async function setGreeting() {}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
